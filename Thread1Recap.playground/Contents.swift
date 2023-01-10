4f13c0d7b3c8e299335eb28701b0ce2140f9234a
Leon Gell <lgell@lcs.on.ca>
2023-01-10 08:56:19 -0500

    all stored and one computed property done

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,20 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+// MARK: Stored Properties
+    let breed: String
+    let color: String
+    var heightInMeters: Double
+    var weightInKG: Double
+    let eyeColor: String
+    let spots: Bool
+    
+    
+//MARK: Computed Properties
+    var heightInCentimeters: Double {
+        return heightInMeters * 100
+    }
+    
+}
+
+var doggo = Dog(breed: <#T##String#>, color: <#T##String#>, heightInMeters: <#T##Double#>, weightInKG: <#T##Double#>, eyeColor: <#T##String#>, spots: false)
